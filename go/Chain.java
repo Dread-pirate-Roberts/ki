7d7e8efe9f2f75b7f72a92cede61d54c7301d962
David Buebe <dbuebe@gmail.com>
2022-07-17 19:02:27 -0500

    Change.java cleanup

--- go/Chain.java
+++ go/Chain.java
@@ -10,62 +10,39 @@ import go.Intersection.piece;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-/**
- */
+
 public class Chain implements Serializable{
 	
-
-
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -7867335357759649093L;
 	protected ArrayList<Location> list;
 	
-	
-	public Chain()
-	{
+	public Chain() {
 		this.list = new ArrayList<Location>();	
 	}
 	
-	public Chain(Chain c)
-	{
+	public Chain(Chain c) {
 		this.list = new ArrayList<Location>(c.list);
 	}
 	
-	public void add(Location l)
-	{
+	public void add(Location l) {
 		this.list.add(l);
 	}
 	
-	public void add(int x, int y)
-	{
-		Location l = new Location();
-		l.x = x;
-		l.y = y;
-		
-		this.list.add(l);
+	public void add(int x, int y) {
+		this.list.add(new Location(x,y));
 	}
 	
-	
-	public boolean contains(int x, int y)
-	{
-		Location l = new Location(x,y);
-		return this.contains(l);
+	public boolean contains(int x, int y) {
+		return this.contains(new Location(x,y));
 	}
 	
-	public boolean contains(Location arg)
-	{
-		boolean ret = false;
-		
-		for(Location l: this.list)
-		{
-			if(l.equals(arg))
-				ret = true;
+	public boolean contains(Location arg) {
+		for(Location l: this.list) {
+			if(l.equals(arg)) {
+				return true;
+			}
 		}
 		
-		return ret;
+		return false;
 	}
-	
 }
